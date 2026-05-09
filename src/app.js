8c408a856fd73d19a2f164339e1edb8cb0b6c87b
Shreyash Jain <2005shreyashjain@gmail.com>
2026-05-09 18:38:48 +0530

    feat: add high-tech landing page for HookFlow root

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const rateLimit = require('express-rate-limit');
 const routes = require('./routes');
 
@@ -16,6 +17,11 @@ app.use(limiter);
 
 
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 
 app.get('/health', (req, res) => {
